0a933c17141a3dcb2b59f51e91a25fb131d94c8f
rniochy <lniochy@gmail.com>
2021-10-23 18:19:37 +0100

    Adicionado a comunicacao a sala

--- public/chat.js
+++ public/chat.js
@@ -1,2 +1,14 @@
 const socket = io();
 
+const urlSearch = new URLSearchParams(window.location.search);
+
+const ursername = urlSearch.get("username");
+const room = urlSearch.get("select_room");
+
+
+
+socket.emit('select_room',{
+     ursername,
+     room,
+})
+console.log(ursername, room);
--- src/webSocket.js
+++ src/webSocket.js
@@ -1,5 +1,16 @@
 import  {io }  from './http.js'
 
+const users = [];
+
 io.on('connection', (socket) =>{
-     console.log(socket.id);
+     socket.on("select_room", data=>{
+          
+          socket.join(data.room);
+
+          users.push({
+                ursername: data.ursername,
+                room: data.room,
+                socket_id: socket.id
+          })
+     });
 });
