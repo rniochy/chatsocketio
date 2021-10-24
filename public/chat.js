d9748a266e63b36b271f77f78b1102acaae3a83b
rniochy <lniochy@gmail.com>
2021-10-24 08:11:17 +0100

    Implementações terminadas!!!

--- public/chat.js
+++ public/chat.js
@@ -5,10 +5,50 @@ const urlSearch = new URLSearchParams(window.location.search);
 const ursername = urlSearch.get("username");
 const room = urlSearch.get("select_room");
 
+document.getElementById("logout").addEventListener("click", (event)=> window.location.href="index.html");
+const userAndRoom = document.getElementById("username");
+userAndRoom.innerHTML = ` 
+   <span> Olá  <strong>${ursername} - você na sala de ${room}</strong> </span> 
+`;
 
 
 socket.emit('select_room',{
      ursername,
      room,
+}, messeges => {
+     messeges.forEach(messege => {
+           createdMessege(messege);
+     });
 })
-console.log(ursername, room);
+
+document.getElementById('message_input')
+.addEventListener('keypress', (event)=>{
+    if(event.key == "Enter"){
+         const messege = event.target.value;
+         
+
+         const data = {
+             room,
+            messege,
+            ursername
+         }
+
+         socket.emit('messege', data);
+         event.target.value = "";
+    }
+
+    socket.on("messege", data=>{
+     createdMessege(data);
+    })
+})
+
+ function createdMessege(data){
+     const messegeDiv = document.getElementById("messages");
+     messegeDiv.innerHTML += ` 
+     <div class="new_message">
+     <label class="form-label">
+         <strong>${data.username}</strong>  <span> ${data.messege} - ${data.createdAt}</span>
+     </label>
+ </div>
+     `;
+ }
--- src/webSocket.js
+++ src/webSocket.js
@@ -4,7 +4,7 @@ const users = [];
 const messeges = [];
 
 io.on('connection', (socket) =>{
-     socket.on("select_room", data=>{
+     socket.on("select_room", (data, callback)=>{
           
           socket.join(data.room);
 
@@ -18,28 +18,36 @@ io.on('connection', (socket) =>{
                     room: data.room,
                     socket_id: socket.id
               });
-
              
-          }          
+          }
+          // All messeges In the for everyUsers 
+          const messegesRoom = getMessegeRoom(data.room);
+          callback(messegesRoom);          
      }); 
      
      socket.on('messege', data => {
+           const date = new Date();
 
             const {username, room, messege} = data;
             const messege_ = {
                   username,
                   room,
                   messege,
-                  createdAt: new Date(),
+                  createdAt: `${date.getUTCDate()}/${date.getMonth() + 1}/${date.getFullYear()} - ${date.getHours()}
+                  :${date.getMinutes()} ` ,
             }
              
             // SAVE THE MAESSEGE
-            console.log(data)
+            messege_.username = data.ursername;
             messeges.push(messege_); 
 
             // SEND TO USER IN THE SAME ROOM
-            console.log(messege_)
             io.to(data.room).emit("messege", messege_);
             
      });
 });
+
+   function getMessegeRoom(room){
+         const messegeRoom = messeges.filter(messege => messege.room === room);
+         return messegeRoom;
+   }
